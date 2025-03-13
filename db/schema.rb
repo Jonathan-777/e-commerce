e07a6111cd26a28a466c87eb80dd0e1767594f7d
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2025-03-13 19:21:38 +0000

    added basic user tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_13_050510) do
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "guest", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+  end
 end
